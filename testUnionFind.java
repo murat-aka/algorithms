e73918a977f4acc1de28582883071a4597d89611
murat-aka <murat-aka@hotmail.com>
2015-11-23 02:35:33 +0000

    update

---
+++ testUnionFind.java
@@ -0,0 +1,33 @@
+
+import edu.princeton.cs.algs4.UF;
+//import java.io.PrintWriter;
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.In;
+
+/**
+ * Reads in a an integer <tt>N</tt> and a sequence of pairs of integers
+ * (between <tt>0</tt> and <tt>N-1</tt>) from standard input, where each integer
+ * in the pair represents some site;
+ * if the sites are in different components, merge the two components
+ * and print the pair to standard output.
+ */
+
+public class testUnionFind{
+    
+    
+        public static void main(String[] args) {
+        int N = StdIn.readInt();
+        UF uf = new UF(N);
+        while (!StdIn.isEmpty()) {
+            int p = StdIn.readInt();
+            int q = StdIn.readInt();
+            if (uf.connected(p, q)) continue;
+            uf.union(p, q);
+            StdOut.println(p + " " + q);
+        }
+        StdOut.println(uf.count() + " components");
+    }
+      
+    
+}
